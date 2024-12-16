dcd46e6d7d7e8c0cd9ac60fe0e96c6761e6489a7
Dmitry Karpovich <92600771+MrKarpovich@users.noreply.github.com>
2024-12-16 08:53:08 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -9,15 +9,35 @@ from aiogram.utils.exceptions import BotBlocked
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import pytz
 
-tg_bot_token = "tg_bot_token"
-open_weather_token = "open_weather_token"
+tg_bot_token = "-"
+open_weather_token = "-"
 
 DANGER_1 = "🟩 Опасности нет, консультативная информация."
-DANGER_2 = "🟨 \n⚠️Обнаружены сильные порывы ветра!"
-DANGER_3 = "🟧 🟧 ВНИМАНИЕ! 🟧 🟧 \n⚠️Опасная погода! Очень сильный ветер, который может нанести ущерб здоровью, привести к экономическим потерям и тратам!"
-DANGER_4 = "🟥🟥🟥 ВНИМАНИЕ!!!🟥🟥🟥 \n⚠️Экстремальная погода! Срочно найти убежище! Погода обязательно нанесет ущерб здоровью и приведет к большим экономическим потерям! НЕМЕДЛЕННО предпринять меры: найти укрытие вдали от деревьев и летающих предметов! ПОВТОРЯЮ! НЕМЕДЛЕННО НАЙТИ УБЕЖИЩЕ!"
+DANGER_2 = "🟨\n⚠️Я объявляю жёлтый уровень тревоги!"
+DANGER_3 = "🟧 🟧 ВНИМАНИЕ!!! 🟧 🟧 \n\n     ⚠️⚠️⚠️Опасная погода! ⚠️⚠️⚠️\n\nОжидается ОЧЕНЬ сильный ветер, который принесет значительный ущерб. ИЗБЕГАЙТЕ ДАВКИ!\n Возможны травмы от падающих деревьев, повреждения зданий, автомобилей и другой собственности, возникнут перебои в подаче электроэнергии и сбои в транспортном движении.\n\n ЧТО ДЕЛАТЬ?! СОВЕТЫ ПО БЕЗОПАСНОСТИ!\n\n НЕМЕДЛЕННО Держаться подальше ОТ деревьев, рекламных щитов и любых других объектов, которые могут снести ветром.\n Закрепите все свои предметы, которые могут быть унесены ветром.\n ПРИ ВОЗМОЖНОСТИ Оставайтесь внутри зданий (где безопасно) и избегайте ненужных поездок.\n ИЗБЕГАЙТЕ ДАВКИ! Думайте своей головой, а не толпой!\n\nБерегите себя и своих близких!\n"
+DANGER_4 = "🟥🟥🟥🟥🟥🟥🟥🟥 \n\n⚠️⚠️⚠️ **ТРЕВОГА!!! ВНИМАНИЕ!!!** ⚠️⚠️⚠️\n\n**Экстремальная погода!! Немедленно найдите укрытие! Торнадо может нанести серьёзный ущерб здоровью и привести к значительным экономическим потерям!\n\nНЕМЕДЛЕННО предпринять меры:\nНайдите укрытие вдали от деревьев, рекламных щитов и любых других объектов, которые могут быть унесены ветром.\nЗакройте окна и двери.\nОставайтесь в безопасных помещениях, таких как подвалы или внутренние комнаты без окон.\nИзбегайте перемещений на улице и не стоите близко к стеклянным и металлическим конструкциям.\n\n**ПОВТОРЯЮ!!! НЕМЕДЛЕННО НАЙДИТЕ УБЕЖИЩЕ!**\n\n**ИЗБЕГАЙТЕ ДАВКИ!** \n\nБерегите себя и своих близких!"
 
-bot = Bot(token=tg_bot_token)
+condition_emojis = {
+    'Thunderstorm': '⛈️⚡',  # Гроза
+    'Lightning': '⛈️⚡',  # Гроза
+    'Hail': '🌨️❄️',  # Град
+    'Snow': '❄️☃️',  # Снег
+    'Fog': '🌫️',  # Туман
+    'Drizzle': '🌧️🌦️',  # Мелкий дождь
+    'Rain': '🌧️☔',  # Дождь
+    'Ice': '🧊❄️',  # Гололёд
+    'Clear': '☀️',  # Ясная погода
+    'Clouds': '☁️',  # Облачно
+    'Tornado': '🌪️',  # Торнадо
+    'Heavy': '💨',  # Сильные осадки/ветер
+    'Freezing Rain': '🌧️❄️',  # Гололёд
+    'Mist': '🌁',  # Мгла
+    'Squall': '💨',  # Шквал
+    'Dust': '🌪️',  # Пыль
+    'Heavy Rain': '🌧️',  # Сильный дождь
+}
+
+bot = Bot(token=tg_bot_token, timeout=120)
 dp = Dispatcher(bot)
 
 # Создание или подключение к базе данных SQLite
@@ -47,8 +67,24 @@ async def add_or_update_city(message: types.Message):
     city = message.text.strip()
 
     if validate_city(city):
-        # Добавление или обновление города пользователя
-        cur.execute("REPLACE INTO users (user_id, city) VALUES (?, ?)", (user_id, city))
+        # Проверка, существует ли запись с таким user_id
+        cur.execute("SELECT id FROM users WHERE user_id = ?", (user_id,))
+        existing_user = cur.fetchone()
+
+        if existing_user:
+            # Обновление города и очистка времен для уровня предупреждений
+            cur.execute("""
+                    UPDATE users
+                    SET city = ?, last_alert_time_lvl_2 = NULL, last_alert_time_lvl_3 = NULL, last_alert_time_lvl_4 = NULL
+                    WHERE user_id = ?
+                """, (city, user_id))
+        else:
+            # Добавление нового пользователя с очисткой времен предупреждений
+            cur.execute("""
+                    INSERT INTO users (user_id, city, last_alert_time_lvl_2, last_alert_time_lvl_3, last_alert_time_lvl_4)
+                    VALUES (?, ?, NULL, NULL, NULL)
+                """, (user_id, city))
+
         conn.commit()
         await message.reply(f"Город {city} был добавлен/обновлен. Теперь я буду следить за погодой в этом городе.")
     else:
@@ -96,13 +132,15 @@ def get_danger_level(temp, wind, pop, humidity, weather_conditions):
 
     # Проверка на наличие опасных погодных условий
     hazardous_weather = any(
-        condition['main'] in ['Thunderstorm', 'Hail'] or 'heavy' in condition['description'].lower()
+        condition['main'] in ['Thunderstorm', 'Hail', 'Fog', 'Tornado', 'Squall', 'freezing rain', 'Severe Winds',
+                              'shower rain', 'sleet', 'light shower sleet', 'shower sleet', 'Mist', 'ash', 'Flooding',
+                              'Lightning', 'Blizzards'] or 'heavy' in condition['description'].lower()
         for condition in weather_conditions
     )
 
     if wind > 20:
         return danger_levels["lvl_4"]
-    elif wind > 15 or (temp > 35 and humidity > 80):
+    elif wind > 15 or (temp > 33 and humidity > 80):
         return danger_levels["lvl_3"]
     elif wind > 9 or hazardous_weather:
         return danger_levels["lvl_2"]
@@ -168,18 +206,20 @@ def get_forecast_for_next_days(forecast_data):
 async def send_weather_warning(user_id, city, danger_level, weather_details, forecast_time):
     hazardous_conditions = []
     for condition in weather_details["weather_conditions"]:
-        if condition['main'] in ['Thunderstorm', 'Hail', 'Snow'] or 'heavy' in condition['description'].lower():
-            hazardous_conditions.append(condition['description'])
+        if condition['main'] in ['Thunderstorm', 'Hail', 'Fog', 'Tornado', 'Squall', 'Freezing Rain', 'Severe Winds',
+                                 'Flooding', 'Lightning', 'Blizzards'] or 'heavy' in condition['description'].lower():
+            emoji = condition_emojis.get(condition['main'], '⚠️')  # Использование эмодзи или ⚠️ по умолчанию
+            hazardous_conditions.append(f"{emoji} {condition['description']}")  # Добавление в список опасных условий
 
     hazardous_conditions_text = '\n'.join(hazardous_conditions) if hazardous_conditions else "_________________________"
 
     await bot.send_message(user_id, f"⚠️Обнаружена угроза погоды на {forecast_time} в городе {city}!\n"
-                                    f"Уровень угрозы: {danger_level}\n"
-                                    f"Температура: {weather_details['temp']}°C\n"
-                                    f"Влажность: {weather_details['humidity']}%\n"
-                                    f"Ветер: {weather_details['wind_speed']} м/с ({weather_details['wind_speed_km_h']:.2f} км/ч)\n"
-                                    f"Давление: {weather_details['pressure']} гПа\n"
-                                    f"Вероятность осадков: {weather_details['pop'] * 100}%\n"
+                                    f"🚨 Уровень угрозы: {danger_level}\n"
+                                    f"🌡️ Температура: {weather_details['temp']}°C\n"
+                                    f"💧 Влажность: {weather_details['humidity']}%\n"
+                                    f"💨 Ветер: {weather_details['wind_speed']} м/с ({weather_details['wind_speed_km_h']:.2f} км/ч)\n"
+                                    f"🧭 Давление: {weather_details['pressure']} гПа\n"
+                                    f"🌧❓️ Вероятность осадков: {weather_details['pop'] * 100}%\n"
                                     f"\n{hazardous_conditions_text}")
 
 
@@ -223,19 +263,19 @@ async def check_weather():
                     current_time = datetime.datetime.now()
 
                     if danger_level == DANGER_2:
-                        if not last_alert_time_lvl_2 or (current_time - datetime.datetime.fromisoformat(last_alert_time_lvl_2)).total_seconds() > 32400:
+                        if not last_alert_time_lvl_2 or (current_time - datetime.datetime.fromisoformat(last_alert_time_lvl_2)).total_seconds() > 58000:
                             await send_weather_warning(user_id, city, danger_level, weather_details, forecast_time_str)
                             cur.execute("UPDATE users SET last_alert_time_lvl_2 = ? WHERE user_id = ?", (current_time.isoformat(), user_id))
                             conn.commit()
 
                     elif danger_level == DANGER_3:
-                        if not last_alert_time_lvl_3 or (current_time - datetime.datetime.fromisoformat(last_alert_time_lvl_3)).total_seconds() > 14400:
+                        if not last_alert_time_lvl_3 or (current_time - datetime.datetime.fromisoformat(last_alert_time_lvl_3)).total_seconds() > 30000:
                             await send_weather_warning(user_id, city, danger_level, weather_details, forecast_time_str)
                             cur.execute("UPDATE users SET last_alert_time_lvl_3 = ? WHERE user_id = ?", (current_time.isoformat(), user_id))
                             conn.commit()
 
                     elif danger_level == DANGER_4:
-                        if not last_alert_time_lvl_4 or (current_time - datetime.datetime.fromisoformat(last_alert_time_lvl_4)).total_seconds() > 1800:
+                        if not last_alert_time_lvl_4 or (current_time - datetime.datetime.fromisoformat(last_alert_time_lvl_4)).total_seconds() > 14400:
                             await send_weather_warning(user_id, city, danger_level, weather_details, forecast_time_str)
                             cur.execute("UPDATE users SET last_alert_time_lvl_4 = ? WHERE user_id = ?", (current_time.isoformat(), user_id))
                             conn.commit()
@@ -249,7 +289,7 @@ async def check_weather():
 
 # Настройка планировщика
 scheduler = AsyncIOScheduler()
-scheduler.add_job(check_weather, "interval", seconds=10)
+scheduler.add_job(check_weather, "interval", seconds=120)
 scheduler.start()
 
 if __name__ == '__main__':
