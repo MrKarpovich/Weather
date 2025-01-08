9b60537e79b3e64ca6d6c2cbe121658916929094
Dmitry Karpovich <92600771+MrKarpovich@users.noreply.github.com>
2025-01-08 10:53:13 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,7 +2,6 @@ import requests
 import datetime
 import time
 import sqlite3
-import os
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
@@ -10,8 +9,8 @@ from aiogram.utils.exceptions import BotBlocked
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 import pytz
 
-tg_bot_token = os.getenv("TG_BOT_TOKEN")
-open_weather_token = os.getenv("OPEN_WEATHER_TOKEN")
+tg_bot_token = "-"
+open_weather_token = "-"
 
 DANGER_1 = "🟩 Опасности нет, консультативная информация."
 DANGER_2 = "🟨\n⚠️Я объявляю жёлтый уровень тревоги!"
@@ -19,24 +18,75 @@ DANGER_3 = "🟧 🟧 ВНИМАНИЕ!!! 🟧 🟧 \n\n     ⚠️⚠️⚠️�
 DANGER_4 = "🟥🟥🟥🟥🟥🟥🟥🟥 \n\n⚠️⚠️⚠️ **ТРЕВОГА!!! ВНИМАНИЕ!!!** ⚠️⚠️⚠️\n\n**Экстремальная погода!! Немедленно найдите укрытие! Торнадо может нанести серьёзный ущерб здоровью и привести к значительным экономическим потерям!\n\nНЕМЕДЛЕННО предпринять меры:\nНайдите укрытие вдали от деревьев, рекламных щитов и любых других объектов, которые могут быть унесены ветром.\nЗакройте окна и двери.\nОставайтесь в безопасных помещениях, таких как подвалы или внутренние комнаты без окон.\nИзбегайте перемещений на улице и не стоите близко к стеклянным и металлическим конструкциям.\n\n**ПОВТОРЯЮ!!! НЕМЕДЛЕННО НАЙДИТЕ УБЕЖИЩЕ!**\n\n**ИЗБЕГАЙТЕ ДАВКИ!** \n\nБерегите себя и своих близких!"
 
 condition_emojis = {
-    'Thunderstorm': '⛈️⚡',  # Гроза
-    'Lightning': '⛈️⚡',  # Гроза
-    'Hail': '🌨️❄️',  # Град
-    'Snow': '❄️☃️',  # Снег
-    'Fog': '🌫️',  # Туман
-    'Drizzle': '🌧️🌦️',  # Мелкий дождь
-    'Rain': '🌧️☔',  # Дождь
-    'Ice': '🧊❄️',  # Гололёд
-    'Clear': '☀️',  # Ясная погода
-    'Clouds': '☁️',  # Облачно
-    'Tornado': '🌪️',  # Торнадо
-    'Heavy': '💨',  # Сильные осадки/ветер
-    'Freezing Rain': '🌧️❄️',  # Гололёд
-    'Mist': '🌁',  # Мгла
-    'Squall': '💨',  # Шквал
-    'Dust': '🌪️',  # Пыль
-    'Heavy Rain': '🌧️',  # Сильный дождь
+    # Группа 2xx: Гроза
+    '200': '⚡ Гроза с небольшим дождём',
+    '201': '⚡⚡ Гроза с дождем',
+    '202': '⚡⚡⚡ Гроза с сильным дождем',
+    '210': '🌩️ Небольшая гроза',
+    '211': '🌩️⚡ Гроза',
+    '212': '🌩️🌩️ Сильная гроза',
+    '221': '🌩️⚡🌩️ Рваная гроза',
+    '230': '⚡💧 Гроза с небольшим моросящим дождем',
+    '231': '⚡💧💦 Гроза с моросью',
+    '232': '⚡💧💦💧 Гроза с сильным моросящим дождем',
+
+    # Группа 3xx: Морось
+    '300': '🌧️ Морось слабой интенсивности',
+    '301': '🌧️💧 Морось',
+    '302': '🌧️💧💧 Сильный моросящий дождь',
+    '310': '🌧️💧🌧️ Слабый моросящий дождь',
+    '311': '🌧️🌧️ Моросящий дождь',
+    '312': '🌧️🌧️🌧️ Сильный интенсивный моросящий дождь',
+    '313': '🌧️🌧️💧 Ливневый дождь и морось',
+    '314': '🌧️🌧️🌧️ Сильный ливневый дождь и морось',
+
+    # Группа 5xx: Дождь
+    '500': '🌦️ Небольшой дождь',
+    '501': '🌧️ Умеренный дождь',
+    '502': '🌧️🌧️ Сильный ливень',
+    '503': '🌧️🌧️🌧️ Очень сильный дождь',
+    '504': '🌧️🌧️🌧️🌧️ Сильный дождь',
+    '511': '🌨️❄️ Ледяной дождь',
+    '520': '🌧️💧 Слабый ливень',
+    '521': '🌧️🌧️ Ливень',
+    '522': '🌧️🌧️🌧️ Сильный ливневый дождь',
+    '531': '🌧️🌧️🌧️🌧️ Рваный ливневый дождь',
+
+    # Группа 6xx: Снег
+    '600': '❄ Небольшой снег',
+    '601': '❄️❄Снег',
+    '602': '❄️❄️❄Сильный снегопад',
+    '611': '🌨️❄️ Мокрый снег',
+    '612': '🌨️💧❄️ Небольшой ливень со снегом',
+    '613': '🌨️❄️💧 Ливень со снегом',
+    '615': '❄️💧❄️ Небольшой дождь со снегом',
+    '616': '🌨️❄️ Дождь и снег',
+    '620': '❄️🌨️❄️ Небольшой ливневый снег',
+    '621': '❄️❄️🌨️ Ливневый снег',
+    '622': '❄️❄️❄️🌨️ Сильный ливневый снег',
+
+    # Группа 7xx: Атмосфера
+    '701': '🌫️ Туман',
+    '711': '💨 Дым',
+    '721': '🌫️ Туман',
+    '731': '🌵 Песок',
+    '741': '🌫️ Туман',
+    '751': '🌵 Песок',
+    '761': '💨 Пыль',
+    '762': '💨 Пепел',
+    '771': '🌀 Шквал',
+    '781': '🌪️ Торнадо',
+
+    # Группа 800: Очистка
+    '800': '🌞 Ясное небо <10%',
+
+    # Группа 80x: Облака
+    '801': '🌤️ Небольшая облачность 11-25%',
+    '802': '⛅ Рассеянная облачность 25-50%',
+    '803': '🌥️ Разорванная облачность 51-84%',
+    '804': '☁ Облачность 85-100%',
 }
+
 
 bot = Bot(token=tg_bot_token, timeout=120)
 dp = Dispatcher(bot)
@@ -99,18 +149,25 @@ def validate_city(city):
         )
         data = response.json()
         return data.get("cod") == 200
-    except Exception as e:
+    except requests.exceptions.RequestException as error_validate_city:
+        # Записываем ошибку при запросе
+        print(f"Ошибка запроса validate_city: {error_validate_city}")
+        return False
+    except Exception as error_validate_city:
+        # Записываем другие исключения
+        print(f"Неизвестная ошибка validate_city: {error_validate_city}")
         return False
 
 
-def get_weather_data(city):
-    try:
-        response = requests.get(
-            f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
-        )
-        return response.json()
-    except Exception as e:
-        raise Exception(f"Ошибка получения данных о погоде: {e}")
+# Это глянуть погоду в текущую секунду, что не надо
+# def get_weather_data(city):
+#     try:
+#         response = requests.get(
+#             f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
+#         )
+#         return response.json()
+#     except Exception as e:
+#         raise Exception(f"Ошибка получения данных о погоде: {e}")
 
 
 def get_forecast_data(city):
@@ -119,8 +176,8 @@ def get_forecast_data(city):
             f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={open_weather_token}&units=metric"
         )
         return response.json()
-    except Exception as e:
-        raise Exception(f"Ошибка получения данных о прогнозе: {e}")
+    except Exception as error_get_forecast_data:
+        print(f"Ошибка при получении прогноза в get_forecast_data: {error_get_forecast_data}")
 
 
 def get_danger_level(temp, wind, pop, humidity, weather_conditions):
@@ -133,17 +190,20 @@ def get_danger_level(temp, wind, pop, humidity, weather_conditions):
 
     # Проверка на наличие опасных погодных условий
     hazardous_weather = any(
-        condition['main'] in ['Thunderstorm', 'Hail', 'Fog', 'Tornado', 'Squall', 'freezing rain', 'Severe Winds',
-                              'shower rain', 'sleet', 'light shower sleet', 'shower sleet', 'Mist', 'ash', 'Flooding',
-                              'Lightning', 'Blizzards'] or 'heavy' in condition['description'].lower()
+        200 <= condition['id'] <= 232 or  # Гроза (Thunderstorm)
+        condition['id'] in [310, 312, 313, 314] or  # Мелкий дождик (Drizzle)
+        502 <= condition['id'] <= 511 or  # Дождь (Rain)
+        condition['id'] in [521, 522, 531] or
+        602 <= condition['id'] <= 622 or  # Снег (Snow)
+        701 <= condition['id'] <= 781  # Атмосферные условия (Mist, Fog, Smoke, Dust, Squall, Tornado)
         for condition in weather_conditions
     )
 
     if wind > 20:
         return danger_levels["lvl_4"]
-    elif wind > 15 or (temp > 33 and humidity > 80):
+    elif wind > 15:
         return danger_levels["lvl_3"]
-    elif wind > 9 or hazardous_weather:
+    elif wind > 9 or (temp > 31 and humidity > 80) or hazardous_weather:
         return danger_levels["lvl_2"]
     else:
         return danger_levels["lvl_1"]
@@ -160,7 +220,7 @@ def clean_database():
 
 
 def convert_wind_speed(speed):
-    return speed * 3.6  # Конвертация м/с в км/ч
+    return speed * 3.6  # Конвертация м/с --> км/ч
 
 
 def get_sun_times(sunrise, sunset):
@@ -206,22 +266,26 @@ def get_forecast_for_next_days(forecast_data):
 
 async def send_weather_warning(user_id, city, danger_level, weather_details, forecast_time):
     hazardous_conditions = []
+    # Для каждого погодного условия в прогнозе
     for condition in weather_details["weather_conditions"]:
-        if condition['main'] in ['Thunderstorm', 'Hail', 'Fog', 'Tornado', 'Squall', 'Freezing Rain', 'Severe Winds',
-                                 'Flooding', 'Lightning', 'Blizzards'] or 'heavy' in condition['description'].lower():
-            emoji = condition_emojis.get(condition['main'], '⚠️')  # Использование эмодзи или ⚠️ по умолчанию
-            hazardous_conditions.append(f"{emoji} {condition['description']}")  # Добавление в список опасных условий
+        condition_code = str(condition['id'])  # Получаем код погодного условия как строку
+        # Получаем описание погодного условия, если оно есть в condition_emojis
+        description = condition_emojis.get(condition_code, 'Неизвестное условие')
 
+        hazardous_conditions.append(f"{description}")  # Добавляем описание в список опасных условий
+
+    # Если опасные условия найдены, отображаем их в сообщении
     hazardous_conditions_text = '\n'.join(hazardous_conditions) if hazardous_conditions else "_________________________"
 
-    await bot.send_message(user_id, f"⚠️Обнаружена угроза погоды на {forecast_time} в городе {city}!\n"
+    await bot.send_message(user_id, f"⚠️Обнаружена угроза погоды \n на {forecast_time} в городе {city}!\n"
                                     f"🚨 Уровень угрозы: {danger_level}\n"
                                     f"🌡️ Температура: {weather_details['temp']}°C\n"
                                     f"💧 Влажность: {weather_details['humidity']}%\n"
                                     f"💨 Ветер: {weather_details['wind_speed']} м/с ({weather_details['wind_speed_km_h']:.2f} км/ч)\n"
                                     f"🧭 Давление: {weather_details['pressure']} гПа\n"
                                     f"🌧❓️ Вероятность осадков: {weather_details['pop'] * 100}%\n"
-                                    f"\n{hazardous_conditions_text}")
+                                    f"Обратите внимание на погодные условия:\n{hazardous_conditions_text}")
+
 
 
 def get_weather_details_from_forecast(forecast_entry):
@@ -251,7 +315,7 @@ async def check_weather():
 
     for user_id, city, last_alert_time_lvl_2, last_alert_time_lvl_3, last_alert_time_lvl_4 in users:
         try:
-            weather_data = get_weather_data(city)
+            # weather_data = get_weather_data(city)
             forecast_data = get_forecast_data(city)
 
             forecast_by_day = get_forecast_for_next_days(forecast_data)
@@ -264,19 +328,19 @@ async def check_weather():
                     current_time = datetime.datetime.now()
 
                     if danger_level == DANGER_2:
-                        if not last_alert_time_lvl_2 or (current_time - datetime.datetime.fromisoformat(last_alert_time_lvl_2)).total_seconds() > 58000:
+                        if not last_alert_time_lvl_2 or (current_time - datetime.datetime.fromisoformat(last_alert_time_lvl_2)).total_seconds() > 54000:
                             await send_weather_warning(user_id, city, danger_level, weather_details, forecast_time_str)
                             cur.execute("UPDATE users SET last_alert_time_lvl_2 = ? WHERE user_id = ?", (current_time.isoformat(), user_id))
                             conn.commit()
 
                     elif danger_level == DANGER_3:
-                        if not last_alert_time_lvl_3 or (current_time - datetime.datetime.fromisoformat(last_alert_time_lvl_3)).total_seconds() > 30000:
+                        if not last_alert_time_lvl_3 or (current_time - datetime.datetime.fromisoformat(last_alert_time_lvl_3)).total_seconds() > 28800:
                             await send_weather_warning(user_id, city, danger_level, weather_details, forecast_time_str)
                             cur.execute("UPDATE users SET last_alert_time_lvl_3 = ? WHERE user_id = ?", (current_time.isoformat(), user_id))
                             conn.commit()
 
                     elif danger_level == DANGER_4:
-                        if not last_alert_time_lvl_4 or (current_time - datetime.datetime.fromisoformat(last_alert_time_lvl_4)).total_seconds() > 14400:
+                        if not last_alert_time_lvl_4 or (current_time - datetime.datetime.fromisoformat(last_alert_time_lvl_4)).total_seconds() > 3600:
                             await send_weather_warning(user_id, city, danger_level, weather_details, forecast_time_str)
                             cur.execute("UPDATE users SET last_alert_time_lvl_4 = ? WHERE user_id = ?", (current_time.isoformat(), user_id))
                             conn.commit()
@@ -284,8 +348,8 @@ async def check_weather():
         except BotBlocked:
             cur.execute("DELETE FROM users WHERE user_id = ?", (user_id,))
             conn.commit()
-        except Exception as e:
-            print(f"Ошибка при проверке погоды для {city}: {e}")
+        except Exception as error_check_weather:
+            print(f"Ошибка при проверке погоды для {city}: {error_check_weather}")
 
 
 # Настройка планировщика
@@ -297,5 +361,5 @@ if __name__ == '__main__':
     try:
         executor.start_polling(dp, skip_updates=True)
     except Exception as e:
-        print(f"Бот крашнул с ошибкой: {e}")
+        print(f"Бот с ошибкой: {e}")
         time.sleep(5)  # Подождать 5 секунд перед повторным запуском
